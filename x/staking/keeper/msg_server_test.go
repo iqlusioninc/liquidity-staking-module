857700bb03cbb25d8d1e14aeb030d455df2b8883
junkai121 <junkai121@outlook.com>
2021-10-22 23:51:33 +1000

    convert to table driven test for TokenizeSharesAndRedeem

--- x/staking/keeper/msg_server_test.go
+++ x/staking/keeper/msg_server_test.go
@@ -15,76 +15,157 @@ import (
 func TestTokenizeSharesAndRedeemTokens(t *testing.T) {
 	_, app, ctx := createTestInput(t)
 
-	addrs := simapp.AddTestAddrs(app, ctx, 2, app.StakingKeeper.TokensFromConsensusPower(ctx, 10000))
-	addrAcc1, addrAcc2 := addrs[0], addrs[1]
-	addrVal1, addrVal2 := sdk.ValAddress(addrAcc1), sdk.ValAddress(addrAcc2)
+	testCases := []struct {
+		name                          string
+		delegationAmount              sdk.Int
+		tokenizeShareAmount           sdk.Int
+		redeemAmount                  sdk.Int
+		expTokenizeErr                bool
+		expRedeemErr                  bool
+		prevAccountDelegationExists   bool
+		recordAccountDelegationExists bool
+	}{
+		{
+			name:                          "full amount tokenize and redeem",
+			delegationAmount:              app.StakingKeeper.TokensFromConsensusPower(ctx, 20),
+			tokenizeShareAmount:           app.StakingKeeper.TokensFromConsensusPower(ctx, 20),
+			redeemAmount:                  app.StakingKeeper.TokensFromConsensusPower(ctx, 20),
+			expTokenizeErr:                false,
+			expRedeemErr:                  false,
+			prevAccountDelegationExists:   false,
+			recordAccountDelegationExists: false,
+		},
+		{
+			name:                          "full amount tokenize and partial redeem",
+			delegationAmount:              app.StakingKeeper.TokensFromConsensusPower(ctx, 20),
+			tokenizeShareAmount:           app.StakingKeeper.TokensFromConsensusPower(ctx, 20),
+			redeemAmount:                  app.StakingKeeper.TokensFromConsensusPower(ctx, 10),
+			expTokenizeErr:                false,
+			expRedeemErr:                  false,
+			prevAccountDelegationExists:   false,
+			recordAccountDelegationExists: true,
+		},
+		{
+			name:                          "partial amount tokenize and full redeem",
+			delegationAmount:              app.StakingKeeper.TokensFromConsensusPower(ctx, 20),
+			tokenizeShareAmount:           app.StakingKeeper.TokensFromConsensusPower(ctx, 10),
+			redeemAmount:                  app.StakingKeeper.TokensFromConsensusPower(ctx, 10),
+			expTokenizeErr:                false,
+			expRedeemErr:                  false,
+			prevAccountDelegationExists:   true,
+			recordAccountDelegationExists: false,
+		},
+		{
+			name:                "over tokenize",
+			delegationAmount:    app.StakingKeeper.TokensFromConsensusPower(ctx, 20),
+			tokenizeShareAmount: app.StakingKeeper.TokensFromConsensusPower(ctx, 30),
+			redeemAmount:        app.StakingKeeper.TokensFromConsensusPower(ctx, 20),
+			expTokenizeErr:      true,
+			expRedeemErr:        false,
+		},
+		{
+			name:                "over redeem",
+			delegationAmount:    app.StakingKeeper.TokensFromConsensusPower(ctx, 20),
+			tokenizeShareAmount: app.StakingKeeper.TokensFromConsensusPower(ctx, 20),
+			redeemAmount:        app.StakingKeeper.TokensFromConsensusPower(ctx, 40),
+			expTokenizeErr:      false,
+			expRedeemErr:        true,
+		},
+	}
 
-	pubKeys := simapp.CreateTestPubKeys(2)
-	pk1, pk2 := pubKeys[0], pubKeys[1]
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, app, ctx = createTestInput(t)
+			addrs := simapp.AddTestAddrs(app, ctx, 2, app.StakingKeeper.TokensFromConsensusPower(ctx, 10000))
+			addrAcc1, addrAcc2 := addrs[0], addrs[1]
+			addrVal1, addrVal2 := sdk.ValAddress(addrAcc1), sdk.ValAddress(addrAcc2)
 
-	// Create Validators and Delegation
-	val1 := teststaking.NewValidator(t, addrVal1, pk1)
-	app.StakingKeeper.SetValidator(ctx, val1)
-	app.StakingKeeper.SetValidatorByPowerIndex(ctx, val1)
+			pubKeys := simapp.CreateTestPubKeys(2)
+			pk1, pk2 := pubKeys[0], pubKeys[1]
 
-	val2 := teststaking.NewValidator(t, addrVal2, pk2)
-	app.StakingKeeper.SetValidator(ctx, val2)
-	app.StakingKeeper.SetValidatorByPowerIndex(ctx, val2)
+			// Create Validators and Delegation
+			val1 := teststaking.NewValidator(t, addrVal1, pk1)
+			app.StakingKeeper.SetValidator(ctx, val1)
+			app.StakingKeeper.SetValidatorByPowerIndex(ctx, val1)
 
-	delTokens := app.StakingKeeper.TokensFromConsensusPower(ctx, 20)
-	err := delegateCoinsFromAccount(ctx, app, addrAcc2, delTokens, val1)
-	require.NoError(t, err)
+			val2 := teststaking.NewValidator(t, addrVal2, pk2)
+			app.StakingKeeper.SetValidator(ctx, val2)
+			app.StakingKeeper.SetValidatorByPowerIndex(ctx, val2)
 
-	// apply TM updates
-	applyValidatorSetUpdates(t, ctx, app.StakingKeeper, -1)
+			delTokens := tc.delegationAmount
+			err := delegateCoinsFromAccount(ctx, app, addrAcc2, delTokens, val1)
+			require.NoError(t, err)
 
-	_, found := app.StakingKeeper.GetDelegation(ctx, addrAcc2, addrVal1)
-	require.True(t, found, "delegation not found after delegate")
+			// apply TM updates
+			applyValidatorSetUpdates(t, ctx, app.StakingKeeper, -1)
 
-	msgServer := keeper.NewMsgServerImpl(app.StakingKeeper)
-	resp, err := msgServer.TokenizeShares(sdk.WrapSDKContext(ctx), &types.MsgTokenizeShares{
-		DelegatorAddress: addrAcc2.String(),
-		ValidatorAddress: addrVal1.String(),
-		Amount:           sdk.NewCoin(app.StakingKeeper.BondDenom(ctx), delTokens),
-	})
-	require.NoError(t, err)
-	_, found = app.StakingKeeper.GetDelegation(ctx, addrAcc2, addrVal1)
-	require.False(t, found, "delegation found after tokenize share")
+			_, found := app.StakingKeeper.GetDelegation(ctx, addrAcc2, addrVal1)
+			require.True(t, found, "delegation not found after delegate")
 
-	shareToken := app.BankKeeper.GetBalance(ctx, addrAcc2, resp.Amount.Denom)
-	require.Equal(t, resp.Amount, shareToken)
-	_, found = app.StakingKeeper.GetValidator(ctx, addrVal1)
-	require.True(t, found, true, "validator not found")
+			msgServer := keeper.NewMsgServerImpl(app.StakingKeeper)
+			resp, err := msgServer.TokenizeShares(sdk.WrapSDKContext(ctx), &types.MsgTokenizeShares{
+				DelegatorAddress: addrAcc2.String(),
+				ValidatorAddress: addrVal1.String(),
+				Amount:           sdk.NewCoin(app.StakingKeeper.BondDenom(ctx), tc.tokenizeShareAmount),
+			})
+			if tc.expTokenizeErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
 
-	records := app.StakingKeeper.GetAllTokenizeShareRecords(ctx)
-	require.Len(t, records, 1)
-	_, found = app.StakingKeeper.GetDelegation(ctx, records[0].GetModuleAddress(), addrVal1)
-	require.True(t, found, "delegation not found from tokenize share module account after tokenize share")
+			if tc.prevAccountDelegationExists {
+				_, found = app.StakingKeeper.GetDelegation(ctx, addrAcc2, addrVal1)
+				require.True(t, found, "delegation found after partial tokenize share")
+			} else {
+				_, found = app.StakingKeeper.GetDelegation(ctx, addrAcc2, addrVal1)
+				require.False(t, found, "delegation found after full tokenize share")
+			}
 
-	// TODO: convert test to table driven
-	// TODO: add partial tokenize share test
-	// TODO: add partial redeem tokens test
+			shareToken := app.BankKeeper.GetBalance(ctx, addrAcc2, resp.Amount.Denom)
+			require.Equal(t, resp.Amount, shareToken)
+			_, found = app.StakingKeeper.GetValidator(ctx, addrVal1)
+			require.True(t, found, true, "validator not found")
 
-	msgServer.RedeemTokens(sdk.WrapSDKContext(ctx), &types.MsgRedeemTokensforShares{
-		DelegatorAddress: addrAcc2.String(),
-		Amount:           resp.Amount,
-	})
-	require.NoError(t, err)
-	delegation, found := app.StakingKeeper.GetDelegation(ctx, addrAcc2, addrVal1)
-	require.True(t, found, "delegation not found after redeem tokens")
-	require.Equal(t, delegation.DelegatorAddress, addrAcc2.String())
-	require.Equal(t, delegation.ValidatorAddress, addrVal1.String())
-	require.Equal(t, delegation.Shares, delTokens.ToDec())
-	shareToken = app.BankKeeper.GetBalance(ctx, addrAcc2, resp.Amount.Denom)
-	require.Equal(t, shareToken.Amount, sdk.ZeroInt())
-	_, found = app.StakingKeeper.GetValidator(ctx, addrVal1)
-	require.True(t, found, true, "validator not found")
+			records := app.StakingKeeper.GetAllTokenizeShareRecords(ctx)
+			require.Len(t, records, 1)
+			_, found = app.StakingKeeper.GetDelegation(ctx, records[0].GetModuleAddress(), addrVal1)
+			require.True(t, found, "delegation not found from tokenize share module account after tokenize share")
 
-	_, found = app.StakingKeeper.GetDelegation(ctx, records[0].GetModuleAddress(), addrVal1)
-	require.False(t, found, "delegation found from tokenize share module account after redeem full amount")
+			_, err = msgServer.RedeemTokens(sdk.WrapSDKContext(ctx), &types.MsgRedeemTokensforShares{
+				DelegatorAddress: addrAcc2.String(),
+				Amount:           sdk.NewCoin(resp.Amount.Denom, tc.redeemAmount),
+			})
+			if tc.expRedeemErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			delegation, found := app.StakingKeeper.GetDelegation(ctx, addrAcc2, addrVal1)
+			require.True(t, found, "delegation not found after redeem tokens")
+			require.Equal(t, delegation.DelegatorAddress, addrAcc2.String())
+			require.Equal(t, delegation.ValidatorAddress, addrVal1.String())
+			require.Equal(t, delegation.Shares, tc.delegationAmount.Sub(tc.tokenizeShareAmount).Add(tc.redeemAmount).ToDec())
+			shareToken = app.BankKeeper.GetBalance(ctx, addrAcc2, resp.Amount.Denom)
+			require.Equal(t, shareToken.Amount.String(), tc.tokenizeShareAmount.Sub(tc.redeemAmount).String())
+			_, found = app.StakingKeeper.GetValidator(ctx, addrVal1)
+			require.True(t, found, true, "validator not found")
 
-	records = app.StakingKeeper.GetAllTokenizeShareRecords(ctx)
-	require.Len(t, records, 0)
+			if tc.recordAccountDelegationExists {
+				_, found = app.StakingKeeper.GetDelegation(ctx, records[0].GetModuleAddress(), addrVal1)
+				require.True(t, found, "delegation not found from tokenize share module account after redeem partial amount")
+
+				records = app.StakingKeeper.GetAllTokenizeShareRecords(ctx)
+				require.Len(t, records, 1)
+			} else {
+				_, found = app.StakingKeeper.GetDelegation(ctx, records[0].GetModuleAddress(), addrVal1)
+				require.False(t, found, "delegation found from tokenize share module account after redeem full amount")
+
+				records = app.StakingKeeper.GetAllTokenizeShareRecords(ctx)
+				require.Len(t, records, 0)
+			}
+		})
+	}
 }
 
 func TestTransferTokenizeShareRecord(t *testing.T) {
