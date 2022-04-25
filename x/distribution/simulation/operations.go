f66f9c5ac16c2f744807945684b58892757727b7
junkai121 <junkai121@outlook.com>
2022-04-26 03:40:01 +1000

    conside auto withdrawn moduleBalance along with operations test fix

--- x/distribution/simulation/operations.go
+++ x/distribution/simulation/operations.go
@@ -15,7 +15,7 @@ import (
 	stakingkeeper "github.com/iqlusioninc/liquidity-staking-module/x/staking/keeper"
 )
 
-const DefaultWeightMsgWithdrawTokenizeShareRecordReward int = 50
+const DefaultWeightMsgWithdrawAllTokenizeShareRecordReward int = 50
 
 // Simulation operation weights constants
 const (
@@ -63,7 +63,7 @@ func WeightedOperations(
 	var weightMsgWithdrawTokenizeShareRecordReward int
 	appParams.GetOrGenerate(cdc, OpWeightMsgWithdrawTokenizeShareRecordReward, &weightMsgWithdrawTokenizeShareRecordReward, nil,
 		func(_ *rand.Rand) {
-			weightMsgWithdrawTokenizeShareRecordReward = DefaultWeightMsgWithdrawTokenizeShareRecordReward
+			weightMsgWithdrawTokenizeShareRecordReward = DefaultWeightMsgWithdrawAllTokenizeShareRecordReward
 		},
 	)
 
