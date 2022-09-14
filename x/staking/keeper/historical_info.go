60969b746d9890dccb81c90132084c07748cab32
vuong177 <nguyenvuong1122000@gmail.com>
2022-09-14 10:24:20 +0700

    Merge branch 'notional/sdkHistoticalInfo'

--- x/staking/keeper/historical_info.go
+++ x/staking/keeper/historical_info.go
@@ -2,11 +2,11 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkstaking "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/iqlusioninc/liquidity-staking-module/x/staking/types"
 )
 
-// GetHistoricalInfo gets the historical info at a given height
-func (k Keeper) GetHistoricalInfo(ctx sdk.Context, height int64) (types.HistoricalInfo, bool) {
+func (k Keeper) GetLiquidStakingHistoricalInfo(ctx sdk.Context, height int64) (types.HistoricalInfo, bool) {
 	store := ctx.KVStore(k.storeKey)
 	key := types.GetHistoricalInfoKey(height)
 
@@ -16,6 +16,19 @@ func (k Keeper) GetHistoricalInfo(ctx sdk.Context, height int64) (types.Historic
 	}
 
 	return types.MustUnmarshalHistoricalInfo(k.cdc, value), true
+}
+
+// GetHistoricalInfo gets the historical info at a given height
+func (k Keeper) GetHistoricalInfo(ctx sdk.Context, height int64) (sdkstaking.HistoricalInfo, bool) {
+	store := ctx.KVStore(k.storeKey)
+	key := types.GetHistoricalInfoKey(height)
+
+	value := store.Get(key)
+	if value == nil {
+		return sdkstaking.HistoricalInfo{}, false
+	}
+
+	return sdkstaking.MustUnmarshalHistoricalInfo(k.cdc, value), true
 }
 
 // SetHistoricalInfo sets the historical info at a given height
