d9273c247403c026695aeb558a244c0c06742cca
junkai121 <junkai121@outlook.com>
2022-10-19 01:23:19 +0800

    migration base

--- x/slashing/migrations/v043/store.go
+++ x/slashing/migrations/v043/store.go
@@ -3,6 +3,8 @@ package v043
 import (
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	v043distribution "github.com/cosmos/cosmos-sdk/x/distribution/migrations/v043"
+	v042slashing "github.com/cosmos/cosmos-sdk/x/slashing/migrations/v042"
 )
 
 // MigrateStore performs in-place store migrations from v0.40 to v0.43. The
@@ -10,5 +12,10 @@ import (
 //
 // - Change addresses to be length-prefixed.
 func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey) error {
+	store := ctx.KVStore(storeKey)
+	v043distribution.MigratePrefixAddress(store, v042slashing.ValidatorSigningInfoKeyPrefix)
+	v043distribution.MigratePrefixAddressBytes(store, v042slashing.ValidatorMissedBlockBitArrayKeyPrefix)
+	v043distribution.MigratePrefixAddress(store, v042slashing.AddrPubkeyRelationKeyPrefix)
+
 	return nil
 }
