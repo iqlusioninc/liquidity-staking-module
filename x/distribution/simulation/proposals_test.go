165b863ead7ad3e1ca0052edfc191831410e7515
Jacob Gadikian <jacobgadikian@gmail.com>
2023-05-23 03:02:03 +0700

    revert changes to proposals test for distribution module

--- x/distribution/simulation/proposals_test.go
+++ x/distribution/simulation/proposals_test.go
@@ -5,38 +5,44 @@ import (
 	"testing"
 
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
-	"gotest.tools/v3/assert"
+	"github.com/stretchr/testify/require"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/address"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	"github.com/cosmos/cosmos-sdk/x/distribution/simulation"
-	"github.com/cosmos/cosmos-sdk/x/distribution/types"
+	simapp "github.com/iqlusioninc/liquidity-staking-module/app"
+	"github.com/iqlusioninc/liquidity-staking-module/x/distribution/simulation"
 )
 
-func TestProposalMsgs(t *testing.T) {
+func TestProposalContents(t *testing.T) {
+	app := simapp.Setup(t, false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
 	// initialize parameters
 	s := rand.NewSource(1)
 	r := rand.New(s)
 
-	ctx := sdk.NewContext(nil, tmproto.Header{}, true, nil)
 	accounts := simtypes.RandomAccounts(r, 3)
 
-	// execute ProposalMsgs function
-	weightedProposalMsgs := simulation.ProposalMsgs()
-	assert.Assert(t, len(weightedProposalMsgs) == 1)
+	// execute ProposalContents function
+	weightedProposalContent := simulation.ProposalContents()
+	require.Len(t, weightedProposalContent, 1)
 
-	w0 := weightedProposalMsgs[0]
+	w0 := weightedProposalContent[0]
 
 	// tests w0 interface:
-	assert.Equal(t, simulation.OpWeightMsgUpdateParams, w0.AppParamsKey())
-	assert.Equal(t, simulation.DefaultWeightMsgUpdateParams, w0.DefaultWeight())
+	require.Equal(t, simulation.OpWeightSubmitCommunitySpendProposal, w0.AppParamsKey())
+	require.Equal(t, simulation.DefaultWeightTextProposal, w0.DefaultWeight())
 
-	msg := w0.MsgSimulatorFn()(r, ctx, accounts)
-	msgUpdateParams, ok := msg.(*types.MsgUpdateParams)
-	assert.Assert(t, ok)
+	amount := sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(1)), sdk.NewCoin("atoken", sdk.NewInt(2)))
 
-	assert.Equal(t, sdk.AccAddress(address.Module("gov")).String(), msgUpdateParams.Authority)
-	assert.DeepEqual(t, sdk.NewDec(0), msgUpdateParams.Params.CommunityTax)
-	assert.Equal(t, true, msgUpdateParams.Params.WithdrawAddrEnabled)
+	feePool := app.DistrKeeper.GetFeePool(ctx)
+	feePool.CommunityPool = sdk.NewDecCoinsFromCoins(amount...)
+	app.DistrKeeper.SetFeePool(ctx, feePool)
+
+	content := w0.ContentSimulatorFn()(r, ctx, accounts)
+
+	require.Equal(t, "sTxPjfweXhSUkMhPjMaxKlMIJMOXcnQfyzeOcbWwNbeHVIkPZBSpYuLyYggwexjxusrBqDOTtGTOWeLrQKjLxzIivHSlcxgdXhhu", content.GetDescription())
+	require.Equal(t, "xKGLwQvuyN", content.GetTitle())
+	require.Equal(t, "distribution", content.ProposalRoute())
+	require.Equal(t, "CommunityPoolSpend", content.ProposalType())
 }
